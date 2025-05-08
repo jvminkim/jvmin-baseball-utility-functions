66fc400a98dbbf2cca4680f6dddcd2bfa5a3fbc6
jvminkim <jaminkim0504@gmail.com>
2025-05-08 18:13:09 -0500

    Reworked data processing

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="baseball-processing",
+    name="baseball-utilities",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
